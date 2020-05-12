0da7b88094ddfb3ae9df55d7f550a474a286efe2
kali <ariary9.2@hotmail.fr>
2020-05-12 04:12:29 -0400

    Re-archi

--- trade_history.py
+++ trade_history.py
@@ -1,7 +1,6 @@
 import pickle
 import kraken_handler
 import trades_handler
-import trades_parser
 
 # RETRIEVING TRADES
 trades_history = {}
@@ -15,11 +14,10 @@ trades_history.update(pickle.load(open("pickles/trades_history.p", "rb")))
 
 
 # PARSING TRADES
-parser = trades_parser.TradesParser(trades_history)
-eth_trades = parser.get_trades_for("XETHZEUR")
+parser = trades_handler.TradesHandler(trades_history)
+parser.get_trades_for("XETHZEUR")
 
 # ANALYZING TRADES
-trades_analyzer = trades_handler.TradesHandler(eth_trades)
-buy, sell = trades_analyzer.get_buy_and_host_costs()
+buy, sell = parser.get_buy_and_host_costs()
 print("BUY: " + str(buy))
 print("SELL: " + str(sell))
--- trades_handler.py
+++ trades_handler.py
@@ -4,6 +4,16 @@ class TradesHandler:
     def __init__(self, trades_dict):
         self.trades_dict = trades_dict
 
+    def get_trades_for(self, currency_str):
+        """return all the trades for a specific currency"""
+        result = {}
+        for trade_id, trade in self.trades_dict.items():
+            pair = trade["pair"]
+            if pair == currency_str:
+                result.update({trade_id: trade})
+
+        self.trades_dict = result
+
     def get_buy_and_host_costs(self):
         """Retrieve the money spend to buy a currency, and the money received when selling it"""
         buy_cost = 0
@@ -16,3 +26,6 @@ class TradesHandler:
                 sell_cost += float(trade["cost"])
 
         return buy_cost, sell_cost
+
+    def get_Trades(self):
+        return self.trades_dict
