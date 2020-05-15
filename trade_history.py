7ef85926717560f41070d54f9161d2b4a4024828
kali <ariary9.2@hotmail.fr>
2020-05-15 11:40:30 -0400

    Add result for all pairs

--- trade_history.py
+++ trade_history.py
@@ -1,4 +1,3 @@
-import datetime
 import pickle
 import kraken_handler
 import trades_handler
@@ -7,19 +6,20 @@ import trades_handler
 trades_history = {}
 
 krakenAPI = kraken_handler.KrakenHandler("./kraken.key")
-start = datetime.datetime(2017, 8, 1)
-trades_history.update(krakenAPI.pull_trades_from(start))
-pickle.dump(trades_history, open("pickles/trades_history_from.p", "wb"))
-th_last_year = krakenAPI.pull_trades_last_year()
-pickle.dump(th_last_year, open("pickles/last_year.p", "wb"))
-# trades_history.update(pickle.load(open("pickles/trades_history_from.p", "rb")))
+trades_history.update(krakenAPI.pull_trades_last_month())
+pickle.dump(trades_history, open("pickles/last_month.p", "wb"))
 
 
 # PARSING TRADES
 parser = trades_handler.TradesHandler(trades_history)
-parser.get_trades_for("XETCZEUR")
+etc_trades = parser.get_trades_for("XETCZEUR")
 
 # ANALYZING TRADES
-buy, sell = parser.get_buy_and_host_costs()
+print("ETC Analysis:")
+buy, sell = parser.get_buy_and_sell_costs(etc_trades)
 print("BUY: " + str(buy))
 print("SELL: " + str(sell))
+print("traded pairs:")
+pairs = parser.get_pairs()
+print(pairs)
+print("Overview:\n" + str(parser.report()))
--- trades_handler.py
+++ trades_handler.py
@@ -3,6 +3,11 @@ class TradesHandler:
 
     def __init__(self, trades_dict):
         self.trades_dict = trades_dict
+        self.pairs = set()
+
+        # Initialize pairs traded
+        for _, trade in self.trades_dict.items():
+            self.pairs.add(trade["pair"])
 
     def get_trades_for(self, currency_str):
         """return all the trades for a specific currency"""
@@ -12,14 +17,14 @@ class TradesHandler:
             if pair == currency_str:
                 result.update({trade_id: trade})
 
-        self.trades_dict = result
+        return result
 
-    def get_buy_and_host_costs(self):
+    def get_buy_and_sell_costs(self, trades):
         """Retrieve the money spend to buy a currency, and the money received when selling it"""
         buy_cost = 0
         sell_cost = 0
 
-        for _, trade in self.trades_dict.items():
+        for _, trade in trades.items():
             if trade["type"] == "buy":
                 buy_cost += float(trade["cost"])
             else:
@@ -27,5 +32,34 @@ class TradesHandler:
 
         return buy_cost, sell_cost
 
-    def get_Trades(self):
+    def get_pairs(self):
+        """ Get the pair trade for the trades history given"""
+        return self.pairs
+
+    def report(self):
+        """
+        Do the same job as get_buy_and_host_costs for all currencies traded
+        TODO: Check if set is empty
+        TODO: Find a right substraction
+        """
+        result = {}
+        result_buy = 0
+        result_sell = 0
+        result_outcome = 0
+        for pair in self.pairs:
+            trades = self.get_trades_for(pair)
+            buy, sell = self.get_buy_and_sell_costs(trades)
+            outcome = sell - buy
+            result_pair = {"buy": buy, "sell": sell, "outcome": outcome}
+            result.update({pair: result_pair})
+            result_buy += buy
+            result_sell += sell
+            result_outcome += outcome
+
+        # Add global results
+        all = {"buy": result_buy, "sell": result_sell, "outcome": result_outcome}
+        result.update({"all": all})
+        return result
+
+    def get_trades(self):
         return self.trades_dict
