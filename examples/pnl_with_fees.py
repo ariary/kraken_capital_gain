8d19610fe075bdfc4b834618b30441cff57e25b8
kali <ariary9.2@hotmail.fr>
2020-05-21 16:16:22 -0400

    Add verbosity

--- examples/pnl_with_fees.py
+++ examples/pnl_with_fees.py
@@ -14,21 +14,29 @@ TAKER_FEE = 0.0026
 # Retrieve open position
 krakenAPI = kraken_handler.KrakenHandler("../kraken.key")
 open_positions = krakenAPI.get_open_positions()
-for _, order in open_positions["result"].items():
-    coin = order["pair"]
-    print("For " + coin + ":")
-    if order["type"] == "sell":
-        print("\t Short position: Vol " + order["vol"])
-    else:
-        print("\t Long position: Vol " + order["vol"])
-    # Fee
-    pnl = float(order["net"])
-    # margin fees (opening + rollover)
-    pnl -= float(order["fee"])
-    # Position fees (open & Close if we do now)
-    opening = (
-        float(order["cost"]) * TAKER_FEE * 2
-    )  # fee opening position (as a taker), 2 for leverage
-    pnl -= opening
-    close_position = float(order["value"]) * 0.0026 * 2
-    print("\t Profit & Loss: " + str(pnl))
+
+if open_positions["result"]:
+    print(
+        "DISCLAIMER:\n\
+        Show an approximation of the positions if you close it with market price now\n"
+    )
+    for _, order in open_positions["result"].items():
+        coin = order["pair"]
+        print("For " + coin + ":")
+        if order["type"] == "sell":
+            print("\t Short position: Vol " + order["vol"])
+        else:
+            print("\t Long position: Vol " + order["vol"])
+        # Fee
+        pnl = float(order["net"])
+        # margin fees (opening + rollover)
+        pnl -= float(order["fee"])
+        # Position fees (open & Close if we do now)
+        opening = (
+            float(order["cost"]) * TAKER_FEE * 2
+        )  # fee opening position (as a taker), 2 for leverage
+        pnl -= opening
+        close_position = float(order["value"]) * 0.0026 * 2
+        print("\t Profit & Loss: " + str(pnl))
+else:
+    print("No position opened")
